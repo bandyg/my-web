da0ef4d5153ed30693a9cda36995e4e96762dabe
bandyg <bandyg@gmail.com>
2023-07-16 15:01:24 +0800

    1 file modified

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,68 +1,66 @@
-import Header from "../components/Header/Header";
-import Startup from "../components/Header/StartupLogo/Startup";
-import MyName from "../components/Home/MyName/MyName";
-import { useContext, useEffect, useState, useRef } from "react";
-import SocialMediaArround from "../components/Home/SocialMediaArround/SocialMediaArround";
-import AboutMe from "../components/Home/AboutMe/AboutMe";
-import ThisCantBeReached from "../components/Home/ThisSiteCantBeReached/ThisCantBeReached";
-import WhereIHaveWorked from "../components/Home/WhereIHaveWorked/WhereIHaveWorked";
-import SomethingIveBuilt from "../components/Home/SomethingIveBuilt/SomethingIveBuilt";
-import GetInTouch from "../components/Home/GetInTouch/GetInTouch";
-import Footer from "../components/Footer/Footer";
-import AppContext from "../components/AppContextFolder/AppContext";
-import Aos from "aos";
-import "aos/dist/aos.css";
-import Head from "next/head";
+import Header from '../components/Header/Header';
+import Startup from '../components/Header/StartupLogo/Startup';
+import MyName from '../components/Home/MyName/MyName';
+import { useContext, useEffect, useState, useRef } from 'react';
+import SocialMediaArround from '../components/Home/SocialMediaArround/SocialMediaArround';
+import AboutMe from '../components/Home/AboutMe/AboutMe';
+import ThisCantBeReached from '../components/Home/ThisSiteCantBeReached/ThisCantBeReached';
+import WhereIHaveWorked from '../components/Home/WhereIHaveWorked/WhereIHaveWorked';
+import SomethingIveBuilt from '../components/Home/SomethingIveBuilt/SomethingIveBuilt';
+import GetInTouch from '../components/Home/GetInTouch/GetInTouch';
+import Footer from '../components/Footer/Footer';
+import AppContext from '../components/AppContextFolder/AppContext';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import Head from 'next/head';
 export default function Home() {
-  const [ShowElement, setShowElement] = useState(false);
-  const [ShowThisCantBeReached, setShowThisCantBeReached] = useState(true);
-  const [ShowMe, setShowMe] = useState(false);
-  // context Variable to clearInterval
-  const context = useContext(AppContext);
-  const aboutRef = useRef<HTMLDivElement>(null);
-  const homeRef = useRef<HTMLDivElement>(null);
+  interface Position {
+    x: number;
+    y: number;
+  }
 
-  useEffect(() => {
-    // remove the interval Cookie timer setter when
-    clearInterval(context.sharedState.userdata.timerCookieRef.current);
-    if (typeof window !== "undefined") {
-      // remove UserDataPuller project EventListeners
-      window.removeEventListener("resize", context.sharedState.userdata.windowSizeTracker.current);
-      window.removeEventListener("mousemove", context.sharedState.userdata.mousePositionTracker.current, false);
-      // remove Typing project EventListeners
-      window.removeEventListener("resize", context.sharedState.typing.eventInputLostFocus);
-      document.removeEventListener("keydown", context.sharedState.typing.keyboardEvent);
+  function getRandomPosition(
+    numPositions: number,
+    imageWidth: number,
+    circleRadius: number
+  ): Position[] {
+    const positions: Position[] = [];
+    const minDistance = 2 * circleRadius; // Minimum distance between circles
+
+    while (positions.length < numPositions) {
+      const x = Math.random() * (imageWidth - circleRadius * 2) + circleRadius;
+      const y = Math.random() * (imageWidth - circleRadius * 2) + circleRadius;
+      const newPosition: Position = { x, y };
+
+      // Check if the new position overlaps with existing positions
+      const isOverlapping = positions.some((position) => {
+        const distance = Math.sqrt(
+          (newPosition.x - position.x) ** 2 + (newPosition.y - position.y) ** 2
+        );
+        return distance < minDistance;
+      });
+
+      if (!isOverlapping) {
+        positions.push(newPosition);
+      }
     }
-    setTimeout(() => {
-      setShowElement(true);
-    }, 4500);
 
-    setTimeout(() => {
-      setShowThisCantBeReached(false);
-    }, 5400);
-    // ? INFORMATIONAL next function will show the component after changing the state of ShowMe
-    setTimeout(() => {
-      setShowElement(false);
-      setShowMe(true);
-      context.sharedState.finishedLoading = true;
-      context.setSharedState(context.sharedState);
-    }, 10400);
-  }, [context, context.sharedState]);
+    return positions;
+  }
+  const numPositions = 10;
+  const imageWidth = 500;
+  const circleRadius = 50;
 
-  useEffect(() => {
-    Aos.init({ duration: 2000, once: true });
-  }, []);
+  const positions = getRandomPosition(numPositions, imageWidth, circleRadius);
 
- 
-
-  console.log("Portfolio Rendered...");
+  console.log('Portfolio Rendered...');
   const meta = {
-    title: "Abdellatif Anaflous - Software Engineer",
+    title: 'Abdellatif Anaflous - Software Engineer',
     description: `I've been working on Software development for 5 years straight. Get in touch with me to know more.`,
-    image: "/titofCercle.png",
-    type: "website",
+    image: '/titofCercle.png',
+    type: 'website',
   };
- 
+
   return (
     <>
       <Head>
@@ -83,12 +81,12 @@ export default function Home() {
         <meta name="twitter:image" content={meta.image} />
       </Head>
       <div className="relative h-screen">
-        <ThisCantBeReached imgPath="/img/titof.jpg"/>
-        <ThisCantBeReached imgPath="/titof.jpg"/>
-        <ThisCantBeReached imgPath="/titof.jpg"/>
-        <ThisCantBeReached imgPath="/titof.jpg"/>
-        <ThisCantBeReached imgPath="/titof.jpg"/>
-        <ThisCantBeReached imgPath="/titof.jpg"/>
+        <ThisCantBeReached imgPath="/img/titof.jpg" />
+        <ThisCantBeReached imgPath="/titof.jpg" />
+        <ThisCantBeReached imgPath="/titof.jpg" />
+        <ThisCantBeReached imgPath="/titof.jpg" />
+        <ThisCantBeReached imgPath="/titof.jpg" />
+        <ThisCantBeReached imgPath="/titof.jpg" />
         {/* {context.sharedState.finishedLoading ? <></> : ShowElement ? <Startup /> : <></>}
         <Header finishedLoading={context.sharedState.finishedLoading} sectionsRef={homeRef} />
         <MyName finishedLoading={context.sharedState.finishedLoading} />
